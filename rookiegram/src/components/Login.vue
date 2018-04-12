324dac05dd433199351beb206ab523bb43e0c604
agrha <agrhakartz@gmail.com>
2018-04-12 22:08:09 +0700

    login

--- rookiegram/src/components/Login.vue
+++ rookiegram/src/components/Login.vue
@@ -1,38 +1,134 @@
 <template>
-  <section class="login-block">
-      <div class="container">
-    <div class="row ">
-      <div class="col login-sec">
-          <h2 class="text-center">Login Now</h2>
-          <form class="login-form">
-    <div class="form-group">
-      <label for="exampleInputEmail1" class="text-uppercase">Username</label>
-      <input type="text" class="form-control" placeholder="">
-      
+<section id="body" style="height: 100vh;">
+    <div style="background-image: url(images/arka.jpg); background-attachment: fixed; background-size: cover; width: 100%; height: 100vh; position: relative;"  >
+    <div class="baslik">
+        <b style="font-size: 70px; text-align: center; margin-bottom: -21px; display: block;">R-GRAM</b>
+        <span style="font-size: 19px; text-align: center; display: block; margin-bottom: 25px;">Upload Here Bro!</span>
     </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1" class="text-uppercase">Password</label>
-      <input type="password" class="form-control" placeholder="">
+    <section>
+    <form method="post" action="">
+        <div class="arkalogin">
+            <div class="loginbaslik">Sign-In</div>
+            <hr style="border: 1px solid #ccc;">
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">Email</span>
+              </div>
+                <input v-model="email" type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">Password</span>
+              </div>
+                <input v-model="password" type="password" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
+            </div>
+            <button type="button" @click="signin" class="btn btn-primary">Login</button>
+            <button type="button" @click="toRegister" class="btn btn-primary">Register</button>
+        </div>
+    </form>
+    </section><br>
+    <span style="font-size: 17px; text-align: center; display: block; color: #fff;
+    ">localhost aja bro</span>
     </div>
-    
-    
-      <div class="form-check">
-      <label class="form-check-label">
-        <input type="checkbox" class="form-check-input">
-        <small>Remember Me</small>
-      </label>
-      <button type="submit" class="btn btn-login float-right">Submit</button>
-    </div>
-    
-  </form>
-    </div>
-      </div>
-      </div>
-  </section>
+    </section>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    signin () {
+      let obj = {
+        email: this.email,
+        password: this.password
+      }
+      axios.post('http://localhost:3000/login', obj)
+        .then(response => {
+          alert('login success')
+          console.log(response.data)
+          localStorage.setItem('token', response.data.token)
+          localStorage.setItem('id', response.data.user._id)
+          localStorage.setItem('email', response.data.user.email)
+          this.$router.push('/timeline')
+        })
+        .catch(err => {
+          alert('login failed')
+          console.log(err.message)
+        })
+    },
+    toRegister () {
+      this.$router.push('/register')
+    }
+  }
 }
 </script>
+
+<style scoped>
+        #body {
+                background: linear-gradient(132deg, #f44336, #E91E63, #9C27B0, #673AB7, #3F51B5, #2196F3,#03A9F4, #00BCD4, #009688, #4CAF50, #FFC107, #FF9800, #f44336, #E91E63, #9C27B0, #673AB7, #3F51B5, #2196F3,#03A9F4, #00BCD4, #009688, #4CAF50, #FFC107, #FF9800);
+                background-size: 400% 400%;
+                animation: BackgroundGradient 30s ease infinite;
+            }
+            @keyframes BackgroundGradient {
+                0% {background-position: 0% 50%;}
+                50% {background-position: 100% 50%;}
+                100% {background-position: 0% 50%;}
+            }
+            .baslik
+{
+    color: #fff;
+    text-align: center;
+    font-family: 'Fira Sans', sans-serif;
+}
+.arkalogin
+{
+    width: 300px;
+    text-align: center;
+    background: #fff;
+    height: 300px;
+    padding: 10px;
+    margin: 50px auto;
+}
+.loginbaslik
+{
+    color: #888888;
+    text-align: left;
+    font-size: 19px;
+    margin-top: 15px;
+}
+.giris
+{
+    width: 100%;
+    height: 40px;
+    margin-top: 10px;
+    border: none;
+    background: #E5E5E5;
+    outline: none;
+    padding: 0 10px;
+}
+.butonlogin
+{
+    width: 100%;
+    margin-top: 10px;
+    height: 40px;
+    font-weight: bold;
+    background: #2196F3;
+    border: none;
+    color: #fff;
+    transition: all 250ms;
+}
+.butonlogin:hover
+{
+    background: #1E88E5;
+}
+#body
+{
+    margin: 0;
+}
+</style>
