cd5ef21ee51ebf4917941545ee4e1b2c9abdb0ed
richoandy <richoandy@gmail.com>
2018-04-12 21:12:52 +0700

    timeline done

--- rookiegram/src/components/Timeline.vue
+++ rookiegram/src/components/Timeline.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="container">
+<div>
+  <Navbar></Navbar>
+  <div class="container mb-5">
     <div class="row">
-        <Item class="margin-right-lg col-lg-4 woggle"></Item>
+        <Item class="margin-right-lg col-lg-4 mb-3"></Item>
         <Item class="margin-right-lg col-lg-4"></Item>
         <Item class="margin-right-lg col-lg-4"></Item>
+        <Item class="margin-right-lg col-lg-4"></Item>
+        <Item class="margin-right-lg col-lg-4"></Item>
+        <Item class="margin-right-lg col-lg-4"></Item>
+        <Item class="margin-right-lg col-lg-4"></Item>
+        <Item class="margin-right-lg col-lg-4"></Item>
+        <Item class="margin-right-lg col-lg-4"></Item>        
       </div>
     </div>
+    <Footer class="fixed-bottom bg-primary"></Footer>
+  </div>
 </template>
 
 <script>
 import Item from '@/components/Item'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 export default {
   components: {
-    Item
+    Item, Navbar, Footer
   }
 }
 </script>
