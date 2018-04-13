f60294764813d8fe7373a737a4d1f1c8b62f288a
Zena <zena.vilenia@gmail.com>
2018-04-13 11:06:03 +0700

    validasi email dan password

--- rookiegram/src/components/Navbar.vue
+++ rookiegram/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-  <a class="navbar-brand text-warning font-weight-bold" href="#" >ROOKIEGRAM</a>
+  <a class="navbar-brand text-warning font-weight-bold" @click="toTimeline">ROOKIEGRAM</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -8,10 +8,10 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto right ">
       <li class="nav-item">
-        <a class="nav-link text-warning" href="#">Home</a>
+        <a class="nav-link text-warning" @click="toTimeline">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-warning" href="#">My Upload</a>
+        <a class="nav-link text-warning" @click="toMyUpload">My Upload</a>
       </li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
@@ -33,6 +33,12 @@ export default {
     logout () {
       localStorage.clear()
       this.$router.push('/')
+    },
+    toMyUpload () {
+      this.$router.push('/myupload')
+    },
+    toTimeline () {
+      this.$router.push('/timeline')
     }
   }
 }
